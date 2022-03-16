8dead569015fb5eaf0ebf15be6d3c0afa42ced65
Marta <martagaworek05@gmail.com>
2022-03-16 15:21:02 +0000

    Provide example setup of environmental variables

---
+++ next.config.sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+      DB_HOST: 'localhost',
+      DB_PORT: 5432, //postgres default port
+      DB_USER: '', //input your username
+      DB_PASSWORD: '', //input your password
+      DB_NAME:'', //input your database name
+  },
+}
